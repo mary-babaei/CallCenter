6558ba78e0934633f1c8cf27959395cdf46bdd20
mary-babaei <158041157+mary-babaei@users.noreply.github.com>
2025-05-01 14:28:07 +0330

    Update analysis_kpis.py

--- analysis_kpis.py
+++ analysis_kpis.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
+
 class KPIAnalyzer:
     def __init__(self, csv_path):
         self.df = pd.read_csv(csv_path)
+        # اطمینان از اینکه ستون Call_Time به فرمت datetime تبدیل بشه
+        self.df['Call_Time'] = pd.to_datetime(self.df['Call_Time'], format='%H:%M:%S').dt.strftime('%H:%M')
 
     def average_handle_time(self):
         return round(self.df['Call_Duration'].mean(), 2)
@@ -20,11 +23,28 @@ class KPIAnalyzer:
         total_calls = len(self.df)
         return round((abandoned_calls / total_calls) * 100, 2)
 
+    def answered_rate(self):
+        answered_calls = self.df['answered'].sum()
+        total_calls = len(self.df)
+        return round((answered_calls / total_calls) * 100, 2)
+
     def average_wait_time(self):
         return round(self.df['Queue_Wait_Time'].mean(), 2)
 
     def call_volume_per_agent(self):
         return self.df['Agent_ID'].value_counts()
+
+    def std_satisfaction(self):
+        return self.df['Customer_Satisfaction'].std()
+
+    def mean_satisfaction(self):
+        return self.df['Customer_Satisfaction'].mean()
+
+    def mean_satisfaction_by_date(self):
+        return self.df.groupby('Call_Date')['Customer_Satisfaction'].mean().round(2)
+
+    def mean_satisfaction_by_time(self):
+        return self.df.groupby('Call_Time')['Customer_Satisfaction'].mean().round(2)
 
     def summary_report(self):
         print("🔍 گزارش KPI مرکز تماس")
@@ -32,6 +52,14 @@ class KPIAnalyzer:
         print(f"2. نرخ حل تماس در اولین تماس (FCR): {self.first_call_resolution_rate()}٪")
         print(f"3. امتیاز رضایت مشتری (CSAT): {self.customer_satisfaction_score()} از 5")
         print(f"4. نرخ قطع تماس (Abandonment Rate): {self.abandonment_rate()}٪")
-        print(f"5. میانگین زمان انتظار: {self.average_wait_time()} دقیقه")
-        print("\n6. تعداد تماس‌ها برای هر اپراتور:")
+        print(f"5. نرخ برقراری تماس (Answered Rate): {self.answered_rate()}٪")
+        print(f"6. میانگین زمان انتظار: {self.average_wait_time()} دقیقه")
+        print(f"📏 انحراف معیار رضایت: {self.std_satisfaction():.2f}")
+        print(f"📏 میانگین رضایت : {self.mean_satisfaction():.2f}")
+        print("📏 میانگین رضایت بر اساس زمان:")
+        print(self.mean_satisfaction_by_time())
+        print("📏 میانگین رضایت بر اساس تاریخ:")
+        print(self.mean_satisfaction_by_date())
+
+        print("\n7. تعداد تماس‌ها برای هر اپراتور:")
         print(self.call_volume_per_agent())
