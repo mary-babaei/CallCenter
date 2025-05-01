c953b21c5636a52f88e8039c19359ef520984b84
mary-babaei <158041157+mary-babaei@users.noreply.github.com>
2025-05-01 14:27:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ from charts import ChartAnalysis
 if __name__ == "__main__":
     # ساخت دیتاست و ذخیره آن در فایل CSV
     generator = GenerateData()
-    df = generator.generate_data()
-    df.to_csv("C:/Users/Maryam/PycharmProjects/pythonProject1/Call_Data_Analysis/call_center_data.csv", index=False)
+    df = generator.generate_data()  # افزودن پرانتز برای فراخوانی متد
+    df.to_csv("C:/Users/Maryam/PycharmProjects/pythonProject1/Call_Data_Analysis/call_center_data_new.csv", index=False)
     print("✅ دیتاست با موفقیت ساخته شد.")
 
     # تحلیل KPIها
-    analyzer = KPIAnalyzer("call_center_data.csv")
-    analyzer.summary_report()
+    analyzer = KPIAnalyzer("C:/Users/Maryam/PycharmProjects/pythonProject1/Call_Data_Analysis/call_center_data_new.csv")
 
     # ذخیره در دیتابیس SQLite
-    storage = DataStorage("call_center_data.csv")
+    storage = DataStorage("C:/Users/Maryam/PycharmProjects/pythonProject1/Call_Data_Analysis/call_center_data_new.csv")
     storage.store_in_sqlite()
 
     # چارت
